c552334e77d93beee5a9cda1295dba3d2642bfff
goodnesslight <evgenijmajboroda1@gmail.com>
2025-04-09 22:03:33 +0300

    Feat: Implemented transaction model and migration

---
+++ app/models/transaction.ts
@@ -0,0 +1,30 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import { TransactionType } from '../common/enums/transaction_type.js'
+import Account from './account.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export default class Transaction extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @belongsTo(() => Account)
+  declare account: BelongsTo<typeof Account>
+  @column()
+  declare accountId: number
+
+  @column()
+  declare type: TransactionType
+
+  @column()
+  declare price: number // decimal
+
+  @column()
+  declare balanceAfter: number // decimal
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ database/migrations/1744223772810_create_transactions_table.ts
@@ -0,0 +1,30 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+import { TransactionType } from '../../app/common/enums/transaction_type.js'
+
+export default class extends BaseSchema {
+  protected tableName = 'transactions'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer('account_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('accounts')
+        .onDelete('CASCADE')
+
+      table.enu('type', Object.values(TransactionType)).notNullable()
+      table.decimal('price', 10, 2)
+      table.decimal('balance_after', 10, 2)
+
+      table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+      table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(this.now())
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
