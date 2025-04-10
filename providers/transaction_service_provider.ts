720bb9a64692a133631ff8c39827a9672caa611a
goodnesslight <evgenijmajboroda1@gmail.com>
2025-04-10 21:35:39 +0300

    Cleanup: Removed unnecessary method

--- providers/transaction_service_provider.ts
+++ providers/transaction_service_provider.ts
@@ -2,16 +2,11 @@ import { TransactionService } from '#services/transaction_service'
 import { ApplicationService } from '@adonisjs/core/types'
 
 export default class TransactionServiceProvider {
-  constructor(protected app: ApplicationService) {}
+  constructor(private readonly app: ApplicationService) {}
 
   public register(): void {
     this.app.container.singleton('app/services/transaction', () => {
       return this.app.container.make(TransactionService)
     })
-  }
-
-  public async ready(): Promise<void> {
-    const service: TransactionService = await this.app.container.make('app/services/transaction')
-    await service.simulate()
   }
 }
